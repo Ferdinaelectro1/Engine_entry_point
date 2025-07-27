f2d9ba19215c3696b6fc690aa4049cf3cc0e88c0
Ferdinand <atiferdinand31@gmail.com>
2025-07-27 21:34:00 +0100

    modif sur la branch test

--- myApp/myapp.cpp
+++ myApp/myapp.cpp
@@ -29,6 +29,19 @@ class OtherApp : public Engine
       };
 };
 
+class TheApp : public Engine
+{
+    public:
+      TheApp(){};
+      void run() override
+      {
+        for(int i = 0;  i < 15; i++)
+        {
+            std::cout << "O_A" << std::endl;        
+        }
+      };
+};
+
 Engine *createEngine()
 {
     return new OtherApp();
